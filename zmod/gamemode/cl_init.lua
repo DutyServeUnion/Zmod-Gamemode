3ffc7d7e0863504a0b686d64c8c5d74a3a47bd4f
Andrew <84892309+DutyServeUnion@users.noreply.github.com>
2021-12-18 18:31:39 +0400

    Delete zmod directory

--- zmod/gamemode/cl_init.lua
+++
@@ -1,7 +0,0 @@
-include("shared.lua")
-
-local function ZMOD()
-	DrawSharpen(1.3, 0.75)
-	DrawBloom(0.65, 0.5, 9, 9, 6, 0, 1.5, 1.5, 1.5)
-end
-hook.Add( "RenderScreenspaceEffects", "ZmodPostProcess", ZMOD )
